f9dd9b0dae1c013444d2bc9bbd7ffdbb22a48d60
Levi Barsis <lbarsis@gmail.com>
2023-05-28 10:05:58 -0700

    changed success url in checkout action

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -2,7 +2,7 @@ class CheckoutsController < ApplicationController
   # Create a PaymentIntent with amount and currency
   def checkout
     session = Stripe::Checkout::Session.create({
-      success_url: 'http://localhost:4000',
+      success_url: 'https://steppers.onrender.com',
       line_items: @current_user.cart_products.map { |cp| {price: cp.product.default_price, quantity: cp.quantity} },
       mode: 'payment',
     })
