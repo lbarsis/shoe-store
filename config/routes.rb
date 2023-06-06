8e05e2fe63be355c65a0b69002e5d94ba32b023d
Levi Barsis <lbarsis@gmail.com>
2023-06-05 19:19:28 -0700

    added Fallback Controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,5 @@ Rails.application.routes.draw do
 
   get '/me', to: 'users#show'
 
-  # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
