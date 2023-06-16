1bd0c3ae71b7849eecb5d079b3ef680400623bba
Levi Barsis <lbarsis@gmail.com>
2023-06-16 15:10:26 -0700

    removed RequestForgeryProtection action

--- app/controllers/stripe_webhooks_controller.rb
+++ app/controllers/stripe_webhooks_controller.rb
@@ -1,5 +1,5 @@
 class StripeWebhooksController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
 
   def create
     payload = request.body.read
